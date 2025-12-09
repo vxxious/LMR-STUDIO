b755ae7d61954a3638de347953ed06d78855b14a
Oshone Omoh <omohoshone5@gmail.com>
2025-12-09 17:02:13 +0100

    fix vite asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-});
-
-
+server: {
+port: 3000
+},
+build: {
+rollupOptions: {
+input: {
+index: resolve(__dirname, 'index.html'),
+booking: resolve(__dirname, 'booking.html'),
+about: resolve(__dirname, 'about.html'),
+contact: resolve(__dirname, 'contact.html'),
+gallery: resolve(__dirname, 'gallery.html'),
+pricing: resolve(__dirname, 'pricing.html'),
+services: resolve(__dirname, 'services.html'),
+policy: resolve(__dirname, 'policy.html')
+}
+}
+}
+})
