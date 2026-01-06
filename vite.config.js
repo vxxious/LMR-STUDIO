4f040f061fc97a4173d5d079e177e42705cbc9ed
Oshone Omoh <omohoshone5@gmail.com>
2026-01-06 03:51:57 +0100

    Fix Vite multi page build and asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite"
+import { resolve } from "path"
 
 export default defineConfig({
-server: {
-port: 3000
-},
-build: {
-rollupOptions: {
-input: {
-index: resolve(__dirname, 'index.html'),
-booking: resolve(__dirname, 'booking.html'),
-about: resolve(__dirname, 'about.html'),
-contact: resolve(__dirname, 'contact.html'),
-gallery: resolve(__dirname, 'gallery.html'),
-pricing: resolve(__dirname, 'pricing.html'),
-services: resolve(__dirname, 'services.html'),
-policy: resolve(__dirname, 'policy.html')
-}
-}
-}
+  server: {
+    port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about/index.html"),
+        booking: resolve(__dirname, "booking/index.html"),
+        contact: resolve(__dirname, "contact/index.html"),
+        gallery: resolve(__dirname, "gallery/index.html"),
+        pricing: resolve(__dirname, "pricing/index.html"),
+        services: resolve(__dirname, "services/index.html"),
+        policy: resolve(__dirname, "policy/index.html")
+      }
+    }
+  }
 })
